e321516adf3024936a2d3f87e8d6457b5c05903f
stuart-wagner <ww289546@icloud.com>
2025-11-04 14:22:32 -0600

    Initial commit

---
+++ diabetes.py
@@ -0,0 +1,29 @@
+''' Using the Diabetes dataset that is in scikit-learn, answer the questions below and create a scatterplot
+graph with a regression line '''
+
+import matplotlib.pylab as plt
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn import datasets
+
+
+#how many sameples and How many features?
+
+
+
+# What does feature s6 represent?
+
+
+
+
+
+#print out the coefficient
+
+
+
+#print out the intercept
+
+
+
+# create a scatterplot with regression line
+
---
+++ iris.py
@@ -0,0 +1,19 @@
+# The Iris dataset is referred to as a “toy dataset” because it has only 150 samples and four features. 
+# The dataset describes 50 samples for each of three Iris flower species—Iris setosa, Iris versicolor and Iris 
+# virginica. Each sample’s features are the sepal length, sepal width, petal 
+# length and petal width, all measured in centimeters. The sepals are the larger outer parts of each flower 
+# that protect the smaller inside petals before the flower buds bloom.
+
+#EXERCISE
+# load the iris dataset and use classification
+# to see if the expected and predicted species
+# match up
+
+# display the shape of the data, target and target_names
+
+# display the first 10 predicted and expected results using
+# the species names not the number (using target_names)
+
+# display the values that the model got wrong
+
+# visualize the data using the confusion matrix
